10b20b3a9c9c44627182e4c64b41593bef701826
Erik Kaunismäki <erik.kaum@gmail.com>
2023-06-07 17:55:44 +0300

    debugging

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ import torch
 
 app = Potassium("my_app")
 
+print("print above")
+logging.warn("log above")
+
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
+    
+    print("print in init")
+    logging.warning("log in init")
+
     device = 0 if torch.cuda.is_available() else -1
     model = pipeline('fill-mask', model='bert-base-uncased', device=device)
    
@@ -20,6 +27,10 @@ def init():
 # @app.handler runs for every call
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
+    
+    print("handler print")
+    logging.warning("log handler")
+
     prompt = request.json.get("prompt")
     model = context.get("model")
     outputs = model(prompt)
